029009d59aeef72c40cc8352b43c8279d3f54f88
Crowd Control Effect Pack Synchronization Tool <kat@warp.world>
2026-04-27 17:17:20 +0000

    Crowd Control Effect Pack Synchronization Tool - Automated Commit

--- MCCCursedHaloCE.cs
+++ MCCCursedHaloCE.cs
@@ -135,7 +135,7 @@ public partial class MCCCursedHaloCE : InjectEffectPack
     private void HandleInvalidRequest(EffectRequest request)
     {
         CcLog.Message($"Invalid request: {FinalCode(request)}");
-        Respond(request, EffectStatus.FailPermanent, StandardErrors.UnknownEffect, request);
+        Respond(request, EffectStatus.FailPermanent, StandardErrors.EffectUnknown, request);
 
     }
 
